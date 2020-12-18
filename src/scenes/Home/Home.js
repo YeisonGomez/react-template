e3d0cc3bd5a31386906515dae8b58a693fdab251
Yeisom Gomez <yeisom40@gmail.com>
2020-12-18 01:11:54 -0500

    feat(package): add slint

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 export const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+          <h1>Home</h1>
     </div>
   )
 }
--- src/scenes/Layout/Public/Public.js
+++ src/scenes/Layout/Public/Public.js
@@ -2,12 +2,13 @@ import * as React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Auth } from '../../Auth/Auth'
+import { Home } from '../../Home/Home'
 
 export const Public = () => {
   return (
     <Router>
       <Switch className="h-100">
-        <Route exact path="/" component={Auth} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   )
