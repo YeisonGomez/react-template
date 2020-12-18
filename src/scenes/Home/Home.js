f91459ef5cf97ecd26a44f91cd07bdf146b1f4e6
Yeisom Gomez <yeisom40@gmail.com>
2020-12-18 01:12:52 -0500

    refactor(slint): precommit validate

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 
 export const Home = () => {
-  return (
-    <div>
-          <h1>Home</h1>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+};
